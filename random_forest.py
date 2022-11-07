ec6a912a8183a62052c588256a5b98dda1f7d7e7
Anupiya Nugaliyadde <anupiyanugel@gmail.com>
2022-11-08 06:48:25 +0800

    Add files via upload

--- random_forest.py
+++ random_forest.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 class Random_forest(Strategy_forML):
     def MLtrainingClassification(self, xtrain, ytrain):
         clf = RandomForestClassifier(max_depth=2, random_state=0)
-        model_clf = clf.fit(xtrain, ytrain)
+        model_clf = clf.fit(xtrain, ytrain.values.ravel())
         return model_clf
 
     def MLtestingClassification(self, model, xtest):
