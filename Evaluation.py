4b84244ddd35db35241ff6f144168802c99d9b4d
Anupiya Nugaliyadde <anupiyanugel@gmail.com>
2022-11-10 22:24:37 +0800

    Add files via upload

--- Evaluation.py
+++ Evaluation.py
@@ -1,19 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.metrics import accuracy_score,classification_report,plot_confusion_matrix
-import matplotlib.pyplot as plt
+
 
 class Evaluation:
 
-    def __init__(self, ytest, prediction):
+    def __init__(self, ytest:pd.DataFrame, prediction:np.ndarray)->None:
         self.ytest = ytest
         self.prediction = prediction
 
-    def calculating_acc(self):
+    def calculating_acc(self)->None:
         print('Accuracy score is: ',round(accuracy_score(self.ytest, self.prediction),4))
 
-    def classification_result(self):
+    def classification_result(self)->None:
         print('Classification report: \n',classification_report(self.ytest, self.prediction))
         
-    def confusion_metrix_result(self, model, xtest):
+    def confusion_metrix_result(self, model, xtest:pd.DataFrame)->None:
+        print(type(model))
         print('Confusion Matrix: \n',plot_confusion_matrix(model, xtest, self.ytest))
         plt.show()
